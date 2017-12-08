655c325a3ab944a94a3872f63b3ed1f355c63fc3
Addison Higham <ahigham@instructure.com>
2017-12-07 22:57:15 -0700

    make logs info instead of debug

--- src/ELBUpdater.js
+++ src/ELBUpdater.js
@@ -20,15 +20,15 @@ class ELBUpdater {
 
   removeTargets(targetArn, targets, cb) {
     const opts = {TargetGroupArn: targetArn}
-    this.logger.debug(opts, 'Calling describeTargetHealth')
+    this.logger.info(opts, 'Calling describeTargetHealth')
     this._elb.describeTargetHealth(opts, (err, resp) => {
       if (err) return cb(err)
-      this.logger.debug({input: opts, output: resp}, 'respose describeTargetHealth')
+      this.logger.info({input: opts, output: resp}, 'respose describeTargetHealth')
       const curTargets = (resp.TargetHealthDescriptions || []).map((t) => t.Target)
       const toRemove = ld.differenceBy(curTargets, targets, (t) => `${t.Id}:${t.Port}`)
 
       if (toRemove.length === 0) return cb()
-      this.logger.debug({targetArn, toRemove}, 'removing targets')
+      this.logger.info({targetArn, toRemove}, 'removing targets')
       this._elb.deregisterTargets({
         TargetGroupArn: targetArn,
         Targets: toRemove
@@ -38,7 +38,7 @@ class ELBUpdater {
 
   updateTargets(targetArn, targets, cb) {
     if (targets.length === 0) return cb()
-    this.logger.debug({targetArn, targets}, 'updating targets')
+    this.logger.info({targetArn, targets}, 'updating targets')
     this._elb.registerTargets({
       TargetGroupArn: targetArn,
       Targets: targets
